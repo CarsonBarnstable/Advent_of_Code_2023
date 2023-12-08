f844ed833873be51c39f3df9725bfd15820597af
Carson Barnstable <cwbarnstable2.0@gmail.com>
2023-12-08 11:59:19 -0700

    Day 8 Part 1 Solution

--- D08_Haunted_Wasteland/sol1.py
+++ D08_Haunted_Wasteland/sol1.py
@@ -1,5 +1,24 @@
 def main():
-    print()
+    path_to_from = ("AAA", "ZZZ")
+    paths = {}
+    with open("input.txt") as f:
+        choices = f.readline().strip()
+        f.readline()  # empty line
+        for line in f.readlines():
+            # GXF = (XQB, GFH)
+            line = line.strip('\n').strip(')').split(" = (")
+            paths[line[0]] = tuple(line[1].split(", "))
+
+    # traverse from AAA to ZZZ
+    dist_count = 0
+    current_ch = path_to_from[0]
+    while current_ch != path_to_from[1]:
+        direction = 0 if choices[dist_count % len(choices)] == 'L' else 1
+        current_ch = paths[current_ch][direction]
+        dist_count += 1
+
+    # result
+    print(dist_count)
 
 
 if __name__ == "__main__":
