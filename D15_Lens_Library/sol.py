4e491f968660b616556c7ba38dbc5106fd6196b7
Carson Barnstable <cwbarnstable2.0@gmail.com>
2023-12-15 21:39:22 -0700

    Day 15 Part 1 Solution

--- D15_Lens_Library/sol.py
+++ D15_Lens_Library/sol.py
@@ -1,6 +1,25 @@
 def main():
-    print()
-    
+    # getting input
+    sequence = []
+    with open("input.txt") as f:
+        for subseq in f.readline().strip('\n').split(','):
+            sequence.append(subseq)
+
+    # calculating sum of hashes
+    hash_sum = 0
+    for subseq in sequence:
+        hash_sum += s_hash(subseq)
+    # part 1 result
+    print("Part 1:", hash_sum)
+
+
+def s_hash(subseq):
+    # direct HASH algorithm
+    val = 0
+    for char in subseq:
+        val = (val+ord(char))*17 % 256
+    return val
+
 
 if __name__ == "__main__":
     main()
