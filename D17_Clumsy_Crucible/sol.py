e8d5642453cbb264689ae46851aaff16a2346d1b
Carson Barnstable <cwbarnstable2.0@gmail.com>
2024-01-02 20:35:30 -0700

    Day 17 Reading in Input

--- D17_Clumsy_Crucible/sol.py
+++ D17_Clumsy_Crucible/sol.py
@@ -1,5 +1,18 @@
+direction = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+
+
 def main():
-    print()
+    # getting input
+    grid = get_input_from("input.txt")
+
+
+
+
+
+def get_input_from(filename):
+    with open(filename) as f:
+        lines = [line.strip('\n') for line in f.readlines()]
+    return [[int(num) for num in line] for line in lines]
 
 
 if __name__ == "__main__":
