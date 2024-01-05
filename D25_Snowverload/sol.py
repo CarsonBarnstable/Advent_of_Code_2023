3a97f8c10e6037a342713acf504ea4691c75f535
Carson Barnstable <cwbarnstable2.0@gmail.com>
2024-01-04 21:26:20 -0700

    Day 25 Input Getting

--- D25_Snowverload/sol.py
+++ D25_Snowverload/sol.py
@@ -1,5 +1,26 @@
 def main():
-    print()
+    lines = []
+    # input
+    with open("input.txt") as f:
+        for line in f.readlines():
+            lines.append(line.strip('\n'))
+
+    # node titles
+    nodes = [line.split(": ")[0] for line in lines]
+    # adjacencies
+    adj = {nodes[i]: [to for to in line.split(": ")[1].split(' ')] for i, line in enumerate(lines)}
+    full_adj = {}
+    for _from, _to in adj.items():
+        for target in _to:
+            if target not in full_adj:
+                full_adj[target] = []
+            full_adj[target].append(_from)
+        if _from not in full_adj:
+            full_adj[_from] = []
+        for t in _to:
+            full_adj[_from].append(t)
+
+    print(full_adj)
 
 
 if __name__ == "__main__":
